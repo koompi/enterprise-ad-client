aa9ad33e3b9afb01da8b96387de92efd28cf7edc
hongsea <hongsea119@gmail.com>
2020-10-01 10:13:08 +0700

    update

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 clear 
 
-##color
+##.................color...................
 RED='\033[0;31m'
 GREEN='\e[32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;32m'
 NC='\033[0m'
 
-function readinput(){
+##.................read input..................
+readinput(){
 read -p "Domain: " VAL1
 read -p "IP Address: " IPADDRESS
 
@@ -18,23 +19,24 @@ newsubdomains=$(echo "$VAL1" | awk -F'.' '{print $1}')
 NEWSUBDOMAIN=$(echo "$newsubdomains" | tr '[:lower:]' '[:upper:]')
 }
 
-##...............BANNER...............
-function banner(){
+##....................banner....................
+banner(){
 echo
 BANNER_NAME=$1
 echo -e "${YELLOW}[+] ${BANNER_NAME} "
 echo -e "---------------------------------------------------${NC}"
 }
 
-function check_root(){
+##....................check root user.................
+check_root(){
 if [[ $(id -u) != 0 ]];then 
 echo "This script run as root"
 exit;
 fi 
 }
 
-##..........................INSTALL PACKAGE.......................
-function install_package_base(){
+##..........................install package base.......................
+install_package_base(){
 banner "Install package."
 
     for P in $(cat $(pwd)/package/package_x86_64)
@@ -49,82 +51,85 @@ banner "Install package."
 }
 
 ##...................krb5 rename.......................
-function krb5(){
+krb5(){
 banner "Configure krb5"
 
-    grep -rli DOMAIN $(pwd)/krb5/* | xargs -i@ sed -i s/DOMAIN/$NEWDOMAIN/g @
-    grep -rli domains $(pwd)/krb5/* | xargs -i@ sed -i s/domains/$newdomains/g @
-    grep -rli subdomain $(pwd)/krb5/* | xargs -i@ sed -i s/subdomain/$newsubdomains/g @
+    cp $(pwd)/krb5/krb5.conf /etc/
+    grep -rli DOMAIN /etc/krb5.conf | xargs -i@ sed -i s/DOMAIN/$NEWDOMAIN/g @
+    grep -rli domains /etc/krb5.conf | xargs -i@ sed -i s/domains/$newdomains/g @
+    grep -rli subdomain /etc/krb5.conf | xargs -i@ sed -i s/subdomain/$newsubdomains/g @
     echo -e "${GREEN}[ OK ]${NC} Configuring krb5..."
 
-    sudo cp /etc/krb5.conf /etc/krb5.conf.backup
-    sudo cp $(pwd)/krb5/krb5.conf /etc/krb5.conf
-    echo -e "${GREEN}[ OK ]${NC} Copy config"
 }
 
 ##..................samba rename...................
-function samba(){
+samba(){
 banner "Configure samba"
 
-    grep -rli SUBDOMAIN $(pwd)/samba/smb.conf | xargs -i@ sed -i s/SUBDOMAIN/$NEWSUBDOMAIN/g @
-    grep -rli DOMAIN $(pwd)/samba/smb.conf | xargs -i@ sed -i s/DOMAIN/$NEWDOMAIN/g @
-    grep -rli domains $(pwd)/samba/smb.conf | xargs -i@ sed -i s/domains/$newdomains/g @
-    grep -rli HOSTNAME $(pwd)/samba/smb.conf | xargs -i@ sed -i s/HOSTNAME/$(hostname)/g @
-    echo -e "${GREEN}[ OK ]${NC} configuring samba rename"
+    sudo cp $(pwd)/samba/* /etc/samba/
+    sudo cp $(pwd)/samba/pam_winbind.conf /etc/security/
+    echo -e "${GREEN}[ OK ]${NC} copy config."
 
-    sudo cp $(pwd)/samba/smb.conf /etc/samba/smb.conf
-    sudo cp $(pwd)/samba/pam_winbind.conf /etc/security/pam_winbind.conf
-    echo -e "${GREEN}[ OK ]${NC} Copy config samba"
-    
+    grep -rli SUBDOMAIN /etc/samba/smb.conf | xargs -i@ sed -i s/SUBDOMAIN/$NEWSUBDOMAIN/g @
+    grep -rli DOMAIN /etc/samba/smb.conf | xargs -i@ sed -i s/DOMAIN/$NEWDOMAIN/g @
+    grep -rli domains /etc/samba/smb.conf | xargs -i@ sed -i s/domains/$newdomains/g @
+    grep -rli HOSTNAME /etc/samba/smb.conf | xargs -i@ sed -i s/HOSTNAME/$(hostname)/g @
+    echo -e "${GREEN}[ OK ]${NC} Configuring samba rename"
+
 }
 
 ##.....................pam mount.......................
-function pam_mount(){
+pam_mount(){
 banner "Configure pam_mount"
     
     cp $(pwd)/pam_mount/* /etc/security/
     echo -e "${GREEN}[ OK ]${NC} Copy configure"
 
-    grep -rli DOMAIN $(pwd)/pam_mount | xargs -i@ sed -i s+DOMAIN+${VAL1}+g @
-    grep -rli SDM $(pwd)/pam_mount | xargs -i@ sed -i s+SDM+${NEWSUBDOMAIN}+g @
+    grep -rli DOMAIN /etc/security/pam_mount.conf.xml | xargs -i@ sed -i s+DOMAIN+${VAL1}+g @
+    grep -rli SDM /etc/security/pam_mount.conf.xml | xargs -i@ sed -i s+SDM+${NEWSUBDOMAIN}+g @
     echo -e "${GREEN}[ OK ]${NC} Configure pam_mount"
 }
 ##..................mysmb service..................
-function mysmb(){
+mysmb(){
 banner "Configure samba service"
     
     sudo cp $(pwd)/scripts/mysmb /usr/bin/mysmb
     sudo cp $(pwd)/service/mysmb.service /usr/lib/systemd/system/
     sudo chmod +x /usr/bin/mysmb
-    echo -e "${GREEN}[ OK ]${NC} configuring client"
+    echo -e "${GREEN}[ OK ]${NC} Configuring client"
 }
 
-function nsswitch(){
+##..................nsswitch..................
+nsswitch(){
 banner "Configure nsswitch"
     
     sudo cp $(pwd)/nsswitch/nsswitch.conf /etc/nsswitch.conf
-    echo -e "${GREEN}[ OK ]${NC} configuring nsswitch"
+    echo -e "${GREEN}[ OK ]${NC} Configuring nsswitch"
 }
 
-function pam(){
+##..................pam authentication...............
+pam(){
 banner "Configure pam"
 
     sudo cp $(pwd)/pam.d/* /etc/pam.d/
     echo -e "${GREEN}[ OK ]${NC} Configuring pam.d"
 }
 
-function resolv(){
+##...................resolv..................
+resolv(){
 banner "Configure resolv"
 
     RESOLVCONF_FILE=/etc/resolvconf.conf
     RESOLV_FILE=/etc/resolv.conf
         
-    echo "resolv_conf=/etc/resolv.conf" > ${RESOLVCONF_FILE}
-    echo "search_domain ${VAL1}" >> ${RESOLVCONF_FILE}
+    #resolvconf
+    cp resolv/resolvconf.conf ${RESOLVCONF_FILE}
     echo "name_servers ${IPADDRESS}" >> ${RESOLVCONF_FILE}
+    echo "search_domains ${VAL1}" >> ${RESOLVCONF_FILE}
     echo "name_servers 8.8.8.8" >> ${RESOLVCONF_FILE}
     echo -e "${GREEN}[ OK ]${NC} Configuring resolvconf"
 
+    #resolv
     echo "search ${VAL1}" > ${RESOLV_FILE}
     echo "nameserver ${IPADDRESS}" >> ${RESOLV_FILE}
     echo "nameserver 8.8.8.8" >> ${RESOLV_FILE}
@@ -133,15 +138,17 @@ banner "Configure resolv"
 
 }
 
-function service(){
+##........................stop service...................
+stopservice(){
 banner "Service"
 
     sudo systemctl enable smb nmb winbind mysmb
     sudo systemctl stop smb nmb winbind mysmb
-    echo -e "${GREEN}[ OK ]${NC} stoping service"
+    echo -e "${GREEN}[ OK ]${NC} Stoped service"
 }
 
-function joindomain(){
+##.....................join domain.......................
+joindomain(){
 banner "Join Domain"
 
     domain=$(echo $VAL1 | tr '[:lower:]' '[:upper:]')
@@ -149,6 +156,17 @@ banner "Join Domain"
     sudo net join -U Administrator@$domain
     echo -e "${GREEN}[ OK ]${NC} Join domain successful"
 }
+
+##.......................start service.....................
+startservice(){
+banner "start service"
+
+    sudo systemctl start smb nmb winbind
+    echo -e "${GREEN}[ OK ]${NC} Started service"
+    sudo systemctl status smb nmb winbind
+    sudo  -e "${GREEN}[ OK ]${NC} Status service"
+}
+
 
 ##call function
 check_root
@@ -161,5 +179,6 @@ mysmb
 nsswitch
 pam
 resolv
-service
+stopservice
 joindomain
+startservice
